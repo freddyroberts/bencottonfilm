2147b423c2b5f3967060c5e8ca170b1e48dfc7e3
freddyroberts <froberts@lewiscommunications.com>
2018-02-18 23:14:19 -0600

    add data and htaccess

--- source/data/index.js
+++ source/data/index.js
@@ -6,5 +6,28 @@ module.exports = {
     "description": "",
     "version": package.version || "",
     "environment": process.env.NODE_ENV || "development",
-    "production": (process.env.NODE_ENV === "production")
+    "production": (process.env.NODE_ENV === "production"),
+	"videos": [
+		{
+			"client": "Maclaren Barbers",
+			"title": "Reklam",
+			"description": "Producer",
+			"video": "https://youtu.be/0G291fPr9Yc",
+			"image": ""
+		},
+		{
+			"client": "Ben Cotton",
+			"title": "Show Reel 2017",
+			"description": "",
+			"video": "https://vimeo.com/242926725",
+			"image": ""
+		},
+		{
+			"client": "Nord",
+			"title": "Stage 3 Official Demo",
+			"description": "",
+			"video": "https://youtu.be/tVBKto6Cfqo",
+			"image": ""
+		}
+	]
 }
