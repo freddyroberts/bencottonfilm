7439e431c4596600ca9a51226b5029fdd5df7d7b
freddyroberts <froberts@lewiscommunications.com>
2018-02-19 14:04:19 -0600

    add

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,6 @@
 git checkout production
 npm run build && npm run templates
+git push -f git@github.com:freddyroberts/bencottonfilm.git master:develop
 cp ./source/CNAME ./public
 cd public
 git init
