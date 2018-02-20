6b49fc20538146294b4f3acae554c7bf9901347c
freddyroberts <froberts@lewiscommunications.com>
2018-02-20 13:33:39 -0600

    add sitemap

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,7 @@
 npm run build && npm run templates
 git push -f git@github.com:freddyroberts/bencottonfilm.git master:develop
 cp ./source/CNAME ./public
+cp ./source/templates/sitemap.xml ./public
 cd public
 git init
 git add -A
